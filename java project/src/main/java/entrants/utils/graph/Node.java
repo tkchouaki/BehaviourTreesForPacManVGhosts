8446d94fcec1a17db666c069fa14ef4af1f46d6d
tkchouaki <tkchouaki@icloud.com>
2018-10-24 09:25:50 +0200

    Added Some Info in Node class

--- java project/src/main/java/entrants/utils/graph/Node.java
+++ java project/src/main/java/entrants/utils/graph/Node.java
@@ -1,21 +1,79 @@
 package entrants.utils.graph;
 
+import pacman.game.Constants;
+import pacman.game.Game;
+import pacman.game.info.GameInfo;
+import pacman.game.internal.Maze;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Node {
     private String id;
+    private boolean containsPill;
+    private boolean containsPowerPill;
+    private boolean containsPacMan;
+    private Set<Constants.GHOST> containedGhosts;
 
     public Node(String id)
     {
         this.setId(id);
+        this.containsPill = false;
+        this.containsPowerPill = false;
+        this.containedGhosts = new HashSet<>();
     }
 
+    public boolean containsPill() {
+        return containsPill;
+    }
+
+    public void setContainsPill(boolean containsPill) {
+        this.containsPill = containsPill;
+    }
+
+    public boolean containsPowerPill() {
+        return containsPowerPill;
+    }
+
+    public void setContainsPowerPill(boolean containsPowerPill) {
+        this.containsPowerPill = containsPowerPill;
+    }
+
+    public boolean containsPacMan() {
+        return containsPacMan;
+    }
+
+    public void setContainsPacMan(boolean containsPacMan) {
+        this.containsPacMan = containsPacMan;
+    }
+
+    public Set<Constants.GHOST> getContainedGhosts() {
+        return containedGhosts;
+    }
+
+    public void setContainedGhosts(Set<Constants.GHOST> containedGhosts) {
+        this.containedGhosts = containedGhosts;
+    }
+
+    public String getId()
+    {
+        return this.id;
+    }
 
     public void setId(String id)
     {
         this.id = id.toLowerCase();
     }
 
-    public String getId()
+    @Override
+    public int hashCode()
     {
-        return this.id;
+        return this.id.hashCode();
+    }
+
+    public void addInfo(GameInfo gameInfo, Maze maze, Game game)
+    {
+        pacman.game.internal.Node node = maze.graph[0];
+
     }
 }
