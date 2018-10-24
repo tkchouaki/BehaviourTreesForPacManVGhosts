dcb6f58e9687003a17dd4e4c87a16ba3c9204ad7
tkchouaki <tkchouaki@icloud.com>
2018-10-24 09:41:01 +0200

    Refractored A few things in Node class Added Commons Class with a method that converts Boolean objects to boolean primitive values.

--- java project/src/main/java/entrants/utils/graph/Node.java
+++ java project/src/main/java/entrants/utils/graph/Node.java
@@ -1,5 +1,6 @@
 package entrants.utils.graph;
 
+import entrants.utils.Commons;
 import pacman.game.Constants;
 import pacman.game.Game;
 import pacman.game.info.GameInfo;
@@ -10,33 +11,25 @@ import java.util.Set;
 
 public class Node {
     private String id;
-    private boolean containsPill;
-    private boolean containsPowerPill;
+    private int containedPillId;
+    private int containedPowerPillId;
     private boolean containsPacMan;
     private Set<Constants.GHOST> containedGhosts;
 
     public Node(String id)
     {
         this.setId(id);
-        this.containsPill = false;
-        this.containsPowerPill = false;
+        this.containedPillId = -1;
+        this.containedPowerPillId = -1;
         this.containedGhosts = new HashSet<>();
     }
 
     public boolean containsPill() {
-        return containsPill;
-    }
-
-    public void setContainsPill(boolean containsPill) {
-        this.containsPill = containsPill;
+        return containedPillId >= 0;
     }
 
     public boolean containsPowerPill() {
-        return containsPowerPill;
-    }
-
-    public void setContainsPowerPill(boolean containsPowerPill) {
-        this.containsPowerPill = containsPowerPill;
+        return containedPowerPillId >= 0;
     }
 
     public boolean containsPacMan() {
@@ -53,6 +46,22 @@ public class Node {
 
     public void setContainedGhosts(Set<Constants.GHOST> containedGhosts) {
         this.containedGhosts = containedGhosts;
+    }
+
+    public int getContainedPillId() {
+        return containedPillId;
+    }
+
+    public void setContainedPillId(int containedPillId) {
+        this.containedPillId = containedPillId;
+    }
+
+    public int getContainedPowerPillId() {
+        return containedPowerPillId;
+    }
+
+    public void setContainedPowerPillId(int containedPowerPillId) {
+        this.containedPowerPillId = containedPowerPillId;
     }
 
     public String getId()
@@ -74,6 +83,6 @@ public class Node {
     public void addInfo(GameInfo gameInfo, Maze maze, Game game)
     {
         pacman.game.internal.Node node = maze.graph[0];
-
     }
+
 }
