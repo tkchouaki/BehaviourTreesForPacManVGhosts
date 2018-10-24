d0e2085f26c8d5975cc0e8375d42802dd5e7d5fb
tkchouaki <tkchouaki@icloud.com>
2018-10-24 15:08:38 +0200

    Blinky has a DispayableGraph

--- java project/src/main/java/entrants/ghosts/username/Blinky.java
+++ java project/src/main/java/entrants/ghosts/username/Blinky.java
@@ -2,33 +2,36 @@ package entrants.ghosts.username;
 
 import entrants.utils.Commons;
 import entrants.utils.graph.KnowledgeGraph;
-import entrants.utils.graph.Node;
+import entrants.utils.ui.KnowledgeGraphDisplayer;
 import pacman.controllers.IndividualGhostController;
-import pacman.controllers.MASController;
 import pacman.game.Constants;
 import pacman.game.Game;
-import scala.collection.immutable.Stream;
+
+import java.nio.file.Paths;
 
 /**
  * Created by Piers on 11/11/2015.
  */
 public class Blinky extends IndividualGhostController {
-    private KnowledgeGraph graphe;
+    private KnowledgeGraph graph;
 
     public Blinky() {
         super(Constants.GHOST.BLINKY);
-        this.graphe = null;
+        this.graph = null;
     }
 
     @Override
     public Constants.MOVE getMove(Game game, long timeDue) {
-        if(this.graphe == null)
+        if(this.graph == null)
         {
-            this.graphe = Commons.initKnowledgeGraph(game);
+            KnowledgeGraphDisplayer displayer;
+            this.graph = Commons.initKnowledgeGraph(game);
+            displayer = new KnowledgeGraphDisplayer(this.graph, "file:///" + Paths.get(".").toAbsolutePath().normalize().toString() + "/src/main/java/entrants/utils/ui/kgraph.css");
+            displayer.display();
         }
         else
         {
-            Commons.updateKnowledgeGraph(game, this.graphe);
+            Commons.updateKnowledgeGraph(game, this.graph);
         }
         return Constants.MOVE.DOWN;
     }
