27fde5e7c9ba978028988480f24ca5a8c950454e
tkchouaki <tkchouaki@icloud.com>
2018-10-24 16:41:34 +0200

    BugFix in DiscreteKnowledgeGraph

--- java project/src/main/java/entrants/utils/graph/DiscreteKnowledgeGraph.java
+++ java project/src/main/java/entrants/utils/graph/DiscreteKnowledgeGraph.java
@@ -1,30 +1,47 @@
 package entrants.utils.graph;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * This class is used to simplify a KnowledgeGraph by only taking into account the interesting nodes
+ */
 public class DiscreteKnowledgeGraph extends KnowledgeGraph{
     private KnowledgeGraph graph;
 
+    /**
+     * Initializes the discrete graph with a target KnowledgeGraph.
+     * At initialization, All the nodes of the target KnowledgeGraph are checked (can be time consuming)
+     * @param graph
+     * The target graph
+     */
     public DiscreteKnowledgeGraph(KnowledgeGraph graph)
     {
         this.graph = graph;
         this.discretizeWholeGraph();
-
     }
 
 
-    public void discretizeWholeGraph()
+    /**
+     * Discritizes the whole target KnowledgeGraph (checking all the nodes)
+     */
+    private void discretizeWholeGraph()
     {
+        //We first clear everything & copy the whole target graph
         this.copyTargetGraph();
-        for(Node node : this.getNodes())
+        //We remove all non interesting nodes & we link all the predecessors of each removed node to its successors
+        for(Node node : this.getNonInterestingNodes())
         {
-            if(!this.isNodeInteresting(node))
-            {
-                this.unlinkAndRemoveNode(node);
-            }
+            this.unlinkAndRemoveNode(node);
         }
     }
 
+    /**
+     * Removes a node & attaches its predecessors to its successors
+     * @param node
+     * The node to remove
+     */
     private void unlinkAndRemoveNode(Node node)
     {
         Collection<Node> successors = this.getSuccessors(node);
@@ -34,11 +51,17 @@ public class DiscreteKnowledgeGraph extends KnowledgeGraph{
         {
             for(Node predecessor : predecessors)
             {
-                this.addDirectedEdge(predecessor, successor);
+                if(!predecessor.equals(successor))
+                {
+                    this.addDirectedEdge(predecessor, successor);
+                }
             }
         }
     }
 
+    /**
+     * Clears everything &nd Copies the whole target KnowledgeGraph
+     */
     private void copyTargetGraph()
     {
         this.emptyGraph();
@@ -49,6 +72,9 @@ public class DiscreteKnowledgeGraph extends KnowledgeGraph{
         }
     }
 
+    /**
+     * Clears everything (removes all the nodes)
+     */
     private void emptyGraph()
     {
         Collection<Node> nodes = this.getNodes();
@@ -59,11 +85,33 @@ public class DiscreteKnowledgeGraph extends KnowledgeGraph{
     }
 
     /**
-     * Checks if a node is interesting (if he should be kept in the DiscreteGraph)
-     * @param node
+     * Retrieves the non interesting Nodes
      * @return
+     * A collection containing the non interesting nodes
      */
-    public boolean isNodeInteresting(Node node)
+    private Collection<Node> getNonInterestingNodes()
+    {
+        Set<Node> result = new HashSet<>();
+        for(Node node : this.getNodes())
+        {
+            if(!this.isNodeInteresting(node))
+            {
+                result.add(node);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Checks if a node is interesting (if he should be kept in the DiscreteGraph)
+     * A node is considered interesting if it contains a Pill, a Power Pill, a PacMan or a Ghost.
+     * It is also considered interesting if its degree is at least 3.
+     * @param node
+     * The node to check
+     * @return
+     * True if the node can be considered interesting, false otherwise
+     */
+    private boolean isNodeInteresting(Node node)
     {
         if(node.containsGhost() || node.containsPacMan())
         {
