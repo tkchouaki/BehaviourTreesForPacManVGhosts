3ecb970fd4774568aed17087b6ddc3d558041333
tkchouaki <tkchouaki@icloud.com>
2018-10-25 13:10:22 +0200

    Added WeightedUndirectedGraph

---
+++ java project/src/main/java/entrants/utils/graph/WeightedUndirectedGraph.java
@@ -0,0 +1,8 @@
+package entrants.utils.graph;
+
+import entrants.utils.graph.interfaces.NodeInterface;
+import entrants.utils.graph.interfaces.WeightedEdgeInterface;
+
+public class WeightedUndirectedGraph<N extends NodeInterface, E extends WeightedEdgeInterface<N>> extends UndirectedGraph<N, E>{
+
+}
