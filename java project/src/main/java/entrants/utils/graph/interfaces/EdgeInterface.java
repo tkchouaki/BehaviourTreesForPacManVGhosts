e48d1798aca9c001af1c308cb7d90bddaedce18c
tkchouaki <tkchouaki@icloud.com>
2018-10-25 07:42:39 +0200

    NodeInterface EdgeInterface

---
+++ java project/src/main/java/entrants/utils/graph/interfaces/EdgeInterface.java
@@ -0,0 +1,7 @@
+package entrants.utils.graph.interfaces;
+
+public interface EdgeInterface{
+    public int getId();
+    public NodeInterface getNodeA();
+    public NodeInterface getNodeB();
+}
---
+++ java project/src/main/java/entrants/utils/graph/interfaces/NodeInterface.java
@@ -0,0 +1,5 @@
+package entrants.utils.graph.interfaces;
+
+public interface NodeInterface extends Comparable{
+    public int getId();
+}
