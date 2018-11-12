4fd7c0d168730c8dac9697d626f4575e5b2ee763
Pierre DUBAILLAY <pierredubaillay@outlook.fr>
2018-11-12 11:13:35 +0100

    Better ui New tests for Nodes

--- java project/src/test/java/entrants/utils/graph/NodeTest.java
+++ java project/src/test/java/entrants/utils/graph/NodeTest.java
@@ -1,11 +1,12 @@
 package entrants.utils.graph;
 
+import entrants.utils.ChangeEventListener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pacman.game.Constants;
 
-import java.util.HashSet;
-import java.util.Set;
+import javax.swing.event.ChangeEvent;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -87,7 +88,7 @@ class NodeTest {
     }
 
     @Test
-    void setContainsPacMan() {
+    void setContainsPacManDescription() {
         node.setContainedPacManDescription(new PacManDescription(new Node(1, 1, 1)));
         assertTrue(node.containsPacMan());
     }
@@ -107,7 +108,7 @@ class NodeTest {
     }
 
     @Test
-    void setContainedGhosts() {
+    void setContainedGhostsDescriptions() {
         Set<GhostDescription> ghosts = new HashSet<>();
         Node n = new Node(1, 1, 1);
         for (Constants.GHOST g : Constants.GHOST.values()) {
@@ -115,28 +116,81 @@ class NodeTest {
         }
         node.setContainedGhostsDescriptions(ghosts);
 
-        assertTrue(ghosts.containsAll(node.getContainedGhosts()));
-        assertTrue(node.getContainedGhosts().containsAll(ghosts));
+        assertTrue(ghosts.containsAll(node.getContainedGhostsDescriptions()));
+        assertTrue(node.getContainedGhostsDescriptions().containsAll(ghosts));
     }
 
     @Test
     void addChangeEventListener() {
+        List<Object> notifications = new ArrayList<>();
+
+        // Register a dummy listener
+        node.addChangeEventListener(new ChangeEventListener() {
+            @Override
+            public void changed(ChangeEvent evt) {
+                notifications.add(true);
+            }
+        });
+
+        // Change something
+        node.setContainedPillId(12);
+
+        // Check if we received a notification
+        assertEquals(1, notifications.size());
     }
 
     @Test
     void removeChangeEventListener() {
+        List<Object> notifications = new ArrayList<>();
+
+        // Register a dummy listener
+        ChangeEventListener listener = new ChangeEventListener() {
+            @Override
+            public void changed(ChangeEvent evt) {
+                notifications.add(true);
+            }
+        };
+        node.addChangeEventListener(listener);
+
+        // Remove it
+        node.removeChangeEventListener(listener);
+
+        // Be sure nothing is received
+        assertEquals(0, notifications.size());
     }
 
     @Test
     void compareTo() {
+        assertEquals(-1, node.compareTo(new Node(1, 1, 1)));
+        assertEquals(Integer.MIN_VALUE, node.compareTo(new Object()));
     }
 
     @Test
     void getNodeById() {
+        List<Node> nodes = new ArrayList<>(Arrays.asList(
+                new Node(0, 0, 0),
+                new Node(1, 1, 0),
+                new Node(2, 0, 1),
+                new Node(3, 1, 1)
+        ));
+        for (int i = 0; i < 4; ++i) {
+            assertEquals(nodes.get(0), Node.getNodeById(nodes, 0));
+        }
+        assertNull(Node.getNodeById(nodes, 12));
     }
 
     @Test
     void getNodesWithPills() {
+        List<Node> nodes = new ArrayList<>(Arrays.asList(
+                new Node(0, 0, 0),
+                new Node(1, 1, 0),
+                new Node(2, 0, 1),
+                new Node(3, 1, 1)
+        ));
+        assertEquals(0, Node.getNodesWithPills(nodes).size());
+        for (Node node : nodes) {
+            node.setContainedPillId(1);
+        }
     }
 
     @Test
