af235a031e093703e342268b0989a40188f95714
Pierre DUBAILLAY <pierredubaillay@outlook.fr>
2018-11-12 09:11:03 +0100

    Updated tests

--- java project/src/test/java/entrants/utils/graph/NodeTest.java
+++ java project/src/test/java/entrants/utils/graph/NodeTest.java
@@ -2,6 +2,11 @@ package entrants.utils.graph;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pacman.game.Constants;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -45,46 +50,74 @@ class NodeTest {
 
     @Test
     void containsPill() {
+        assertFalse(node.containsPill());
     }
 
     @Test
     void getContainedPillId() {
+        assertFalse(node.containsPowerPill());
     }
 
     @Test
     void setContainedPillId() {
+        node.setContainedPillId(12);
+        assertTrue(node.containsPill());
+        assertEquals(12, node.getContainedPillId());
     }
 
     @Test
     void containsPowerPill() {
+        node.setContainedPowerPillId(12);
+        assertTrue(node.containsPowerPill());
+        assertEquals(12, node.getContainedPowerPillId());
     }
 
     @Test
     void getContainedPowerPillId() {
+        assertEquals(-1, node.getContainedPillId());
     }
 
     @Test
     void setContainedPowerPillId() {
+        assertEquals(-1, node.getContainedPowerPillId());
     }
 
     @Test
     void containsPacMan() {
+        assertFalse(node.containsPacMan());
     }
 
     @Test
     void setContainsPacMan() {
+        node.setContainedPacMan(new PacManDescription(new Node(1, 1, 1)));
+        assertTrue(node.containsPacMan());
     }
 
     @Test
     void containsGhost() {
+        assertFalse(node.containsGhost());
+        for (Constants.GHOST ghost : Constants.GHOST.values()) {
+            assertFalse(node.containsGhost(ghost));
+        }
     }
 
     @Test
     void getContainedGhosts() {
+        assertNotNull(node.getContainedGhosts());
+        assertEquals(0, node.getContainedGhosts().size());
     }
 
     @Test
     void setContainedGhosts() {
+        Set<GhostDescription> ghosts = new HashSet<>();
+        Node n = new Node(1, 1, 1);
+        for (Constants.GHOST g : Constants.GHOST.values()) {
+            ghosts.add(new GhostDescription(g, n));
+        }
+        node.setContainedGhosts(ghosts);
+
+        assertTrue(ghosts.containsAll(node.getContainedGhosts()));
+        assertTrue(node.getContainedGhosts().containsAll(ghosts));
     }
 
     @Test
